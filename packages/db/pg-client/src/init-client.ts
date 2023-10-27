c9a8a8acb26301d939d8b020311b545c1bcaa542
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2023-10-27 11:14:15 -0500

    refactor: support passing in sql instance
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

--- packages/db/pg-client/src/init-client.ts
+++ packages/db/pg-client/src/init-client.ts
@@ -2,28 +2,31 @@ import { CamelCasePlugin, Kysely } from 'kysely';
 import { PostgresJSDialect } from 'kysely-postgres-js';
 import postgres from 'postgres';
 
-import type { InitClientFn, InitClientOpts, PgClient } from './types.ts';
+import type { InitClientFn, InitClientOpts, InitClientOptsWithSql, PgClient } from './types.ts';
 
 let client: PgClient<any>;
 
-export const initClient: InitClientFn = <DB>({ uri, max, debug, reuse }: InitClientOpts): PgClient<DB> => {
+export const initClient: InitClientFn = <DB>(opts: InitClientOpts): PgClient<DB> => {
+  const { debug, reuse } = opts;
   if (client && reuse) {
     return client as PgClient<DB>;
   }
+
+  const sql = isInitClientOptsWithSql(opts) ? opts.sql : postgres(opts.uri, { max: opts.max });
 
   const db = new Kysely<DB>({
     plugins: [new CamelCasePlugin()],
     log: debug ? ['query', 'error'] : ['error'],
     dialect: new PostgresJSDialect({
-      postgres,
-      connectionString: uri,
-      options: {
-        max,
-      },
+      postgres: sql,
     }),
   });
 
-  client = { db };
+  client = { db, sql };
 
   return client as PgClient<DB>;
+};
+
+const isInitClientOptsWithSql = (opts: InitClientOpts): opts is InitClientOptsWithSql => {
+  return (opts as InitClientOptsWithSql).sql !== undefined;
 };
--- packages/db/pg-client/src/types.ts
+++ packages/db/pg-client/src/types.ts
@@ -1,24 +1,34 @@
 import type { Kysely } from 'kysely';
+import type { Sql } from 'postgres';
 
-export interface InitClientOpts {
-  uri: string;
-
+type BaseInitClientOpts = {
   debug?: boolean;
-
-  /**
-   * Max connections, for the clients that support it.
-   */
-  max?: number;
 
   /**
    * Re-use existing client, if one was created previously.
    * Only applicable in the edge client, and usually you will not want this.
    */
   reuse?: boolean;
-}
+};
+
+export type InitClientOptsWithUri = BaseInitClientOpts & {
+  uri: string;
+
+  /**
+   * Max connections, for the clients that support it.
+   */
+  max?: number;
+};
+
+export type InitClientOptsWithSql = BaseInitClientOpts & {
+  sql: Sql;
+};
+
+export type InitClientOpts = InitClientOptsWithUri | InitClientOptsWithSql;
 
 export interface PgClient<T> {
   db: Kysely<T>;
+  sql: Sql;
 }
 
 export type InitClientFn = <T>(opts: InitClientOpts) => PgClient<T>;
