143691d4137837d30ac44c04a57b00a90e48c77b
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2023-10-27 14:04:55 -0500

    chore: add calendar plugin to dayjs
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

--- packages/utils/dates/src/instance.ts
+++ packages/utils/dates/src/instance.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import calendar from 'dayjs/plugin/calendar.js';
 import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 import relativeTime from 'dayjs/plugin/relativeTime.js';
 import utc from 'dayjs/plugin/utc.js';
@@ -6,5 +7,6 @@ import utc from 'dayjs/plugin/utc.js';
 dayjs.extend(utc);
 dayjs.extend(customParseFormat);
 dayjs.extend(relativeTime);
+dayjs.extend(calendar);
 
 export { dayjs };
