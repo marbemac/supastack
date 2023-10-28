d2061b2532ed6a31fb050785d99d660326518e0d
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2023-10-28 17:34:51 -0500

    fix: typings
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

--- packages/utils/ids/src/hash.ts
+++ packages/utils/ids/src/hash.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error no typings
 import fnv from 'fnv-plus';
 
-export const hash = (str: string) => fnv.fast1a64(str);
-export const hash32 = (str: string) => fnv.fast1a32hex(str);
+export const hash = (str: string) => fnv.fast1a64(str) as string;
+export const hash32 = (str: string) => fnv.fast1a32hex(str) as string;
