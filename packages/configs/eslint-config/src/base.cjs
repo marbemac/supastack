19127de7963f01a3e837673bf135a1ac019f7a2c
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2023-11-09 14:31:20 -0600

    chore: cleanup eslint config a bit
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

--- packages/configs/eslint-config/src/base.cjs
+++ packages/configs/eslint-config/src/base.cjs
@@ -16,20 +16,23 @@ const config = {
     'tailwind-config.ts',
     'tailwind.config.js',
     'postcss.config.js',
+    '*.d.ts',
   ],
 
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended'],
 
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  plugins: ['simple-import-sort', 'import'],
 
-  plugins: ['@typescript-eslint', 'simple-import-sort', 'import'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 
   env: {
+    browser: true,
     es6: true,
   },
 
@@ -47,23 +50,54 @@ const config = {
     'simple-import-sort/exports': 'error',
     'import/extensions': ['error', 'ignorePackages'],
     'no-return-await': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        extendDefaults: true,
-        types: {
-          // un-ban a type that's banned by default
-          Function: false,
-          '{}': false,
-        },
-      },
-    ],
   },
+
+  overrides: [
+    // Typescript
+    {
+      files: ['**/*.{ts,tsx}'],
+      plugins: ['@typescript-eslint'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'prettier',
+      ],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+        '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            extendDefaults: true,
+            types: {
+              // un-ban a type that's banned by default
+              Function: false,
+              '{}': false,
+            },
+          },
+        ],
+      },
+    },
+
+    // Markdown
+    {
+      files: ['**/*.md'],
+      plugins: ['markdown'],
+      extends: ['plugin:markdown/recommended', 'prettier'],
+    },
+
+    // Jest/Vitest
+    {
+      files: ['**/*.(test|spec).{js,jsx,ts,tsx}'],
+      plugins: ['vitest'],
+      extends: ['plugin:vitest/recommended', 'prettier'],
+    },
+  ],
 };
 
 module.exports = config;
--- packages/configs/eslint-config/src/react.cjs
+++ packages/configs/eslint-config/src/react.cjs
@@ -2,22 +2,43 @@
 const config = {
   root: true,
 
-  extends: [
-    '@supastack/eslint-config/base',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['@supastack/eslint-config/base'],
 
-  rules: {
-    'react/prop-types': 'off',
-    'react/no-unknown-property': [
-      2,
-      {
-        ignore: ['jsx', 'global'],
+  overrides: [
+    {
+      files: ['**/*.{js,jsx,ts,tsx}'],
+      plugins: ['react', 'jsx-a11y'],
+
+      extends: [
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
+      ],
+
+      settings: {
+        react: {
+          version: 'detect',
+        },
+        formComponents: ['Form'],
+        linkComponents: [
+          { name: 'Link', linkAttribute: 'to' },
+          { name: 'NavLink', linkAttribute: 'to' },
+        ],
       },
-    ],
-  },
+
+      rules: {
+        'react/prop-types': 'off',
+        'react/no-unknown-property': [
+          2,
+          {
+            ignore: ['jsx', 'global'],
+          },
+        ],
+      },
+    },
+  ],
 };
 
 module.exports = config;
